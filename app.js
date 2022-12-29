6216db78ae68e58f4cc8bd82a9a79effea8c5dcf
ShubhamThakar1822 <121230998+ShubhamThakar1822@users.noreply.github.com>
2022-12-29 19:27:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/",function(req,resp){
-    resp.send("Bhag bhosdike");
+    resp.send("Server is going to ONN mode");
 })
 app.listen(7000);
 console.log("Server chalu zhala...");
